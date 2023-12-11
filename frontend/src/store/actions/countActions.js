2820a7c80daee495385b1a17f2c144245457f19c
navedkhan012 <navedkhan012@gmail.com>
2023-12-11 21:34:56 +0530

    update

--- frontend/src/store/actions/countActions.js
+++ frontend/src/store/actions/countActions.js
@@ -3,7 +3,7 @@ import { countAction } from "../reducers/countReducers";
 export const changeCount = (type) => (dispatch, getState) => {
   const { count } = getState();
 
-  if ((type = "INCREASE")) {
+  if (type === "DECREASE") {
     dispatch(countAction.countChange(count.number + 1));
   } else {
     dispatch(countAction.countChange(count.number - 1));
